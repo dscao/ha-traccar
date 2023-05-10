1f448bd54f40cf64827bda4eb3116cd475c70a1f
ryanhe <=>
2023-05-10 11:39:10 +0800

    添加缺少的文件

---
+++ custom_components/ha_traccar/binary_sensor.py
@@ -0,0 +1,128 @@
+"""Support for Traccar device tracking."""
+from __future__ import annotations
+
+import logging
+
+
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+    BinarySensorEntityDescription,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.const import STATE_ON
+
+from .const import (
+    ATTR_ARMED,
+    ATTR_CHARGE,
+    ATTR_IGNITION,
+    ATTR_MOTION,
+    DOMAIN,
+    KEY_ARMED,
+    KEY_CHARGE,
+    KEY_IGNITION,
+    KEY_MOTION,
+    TRACKER_UPDATE,
+    SENSORS,
+    CONF_SENSORS,
+)
+
+from . import TraccarEntity
+
+BINARY_SENSOR_TYPES: tuple[BinarySensorEntityDescription, ...] = (
+    BinarySensorEntityDescription(
+        key=KEY_MOTION,
+        name="Motion",
+        device_class=BinarySensorDeviceClass.MOTION
+    ),
+    BinarySensorEntityDescription(
+        key=KEY_ARMED,
+        name="Armed",
+        device_class=BinarySensorDeviceClass.SAFETY
+    ),
+    BinarySensorEntityDescription(
+        key=KEY_CHARGE,
+        name="Charge",
+        device_class=BinarySensorDeviceClass.BATTERY_CHARGING
+    ),
+    BinarySensorEntityDescription(
+        key=KEY_IGNITION,
+        name="Ignition",
+        device_class=BinarySensorDeviceClass.POWER
+    ),
+)
+
+BINARY_SENSOR_TYPES_MAP = {
+    description.key: description for description in BINARY_SENSOR_TYPES}
+
+BINARY_SENSOR_TYPES_KEYS = {
+    description.key for description in BINARY_SENSOR_TYPES}
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Configure a dispatcher connection based on a config entry."""
+
+    enabled_sensors = [s for s in entry.data.get(
+        CONF_SENSORS, []) if s in BINARY_SENSOR_TYPES_KEYS]
+
+    @callback
+    def _receive_data(server, device, position):
+        for sensor_type in enabled_sensors:
+            sensor_id = f"{device.unique_id}-{sensor_type}"
+            if sensor_id in hass.data[DOMAIN][entry.entry_id][SENSORS]:
+                continue
+
+            hass.data[DOMAIN][entry.entry_id][SENSORS].add(sensor_id)
+
+            async_add_entities(
+                [TraccarBinarySensorEntity(
+                    BINARY_SENSOR_TYPES_MAP[sensor_type], server, device, position)]
+            )
+
+    async_dispatcher_connect(hass, TRACKER_UPDATE, _receive_data)
+
+
+class TraccarBinarySensorEntity(BinarySensorEntity, TraccarEntity):
+
+    def __init__(self, description, server, device, position):
+        super().__init__(server, device)
+        self.entity_description = description
+        self._unique_id = f"{server}-{device.unique_id}-{description.key}"
+
+        attr_map = {
+            KEY_MOTION: ATTR_MOTION,
+            KEY_ARMED: ATTR_ARMED,
+            KEY_CHARGE: ATTR_CHARGE,
+            KEY_IGNITION: ATTR_IGNITION
+        }
+        self._attributes_key = attr_map.get(description.key)
+        self._update_traccar_info(device, position)
+
+    def _update_traccar_info(self, device, position):
+        self._name = f"{device.name} {self.entity_description.name}"
+        if (_state := position.attributes.get(self._attributes_key)) is not None:
+            self._attr_is_on = _state
+
+    @property
+    def name(self):
+        """Return the name of the device."""
+        return self._name
+
+    @property
+    def unique_id(self):
+        """Return the unique ID."""
+        return self._unique_id
+
+    async def async_added_to_hass(self) -> None:
+        """Register state update callback."""
+        await super().async_added_to_hass()
+
+        if state := await self.async_get_last_state():
+            self._attr_is_on = state.state == STATE_ON
---
+++ custom_components/ha_traccar/sensor.py
@@ -0,0 +1,112 @@
+"""Support for Traccar device tracking."""
+from __future__ import annotations
+
+import logging
+
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.const import UnitOfElectricPotential, PERCENTAGE
+
+from .const import (
+    DOMAIN,
+    TRACKER_UPDATE,
+    SENSORS,
+    CONF_SENSORS,
+    ATTR_BATTERY_LEVEL,
+    ATTR_BATTERY,
+    KEY_BATTERY_LEVEL,
+    KEY_BATTERY,
+)
+
+from . import TraccarEntity
+
+SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
+    SensorEntityDescription(
+        key=KEY_BATTERY_LEVEL,
+        name="Battery Level",
+        device_class=SensorDeviceClass.BATTERY,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=PERCENTAGE
+    ),
+    SensorEntityDescription(
+        key=KEY_BATTERY,
+        name="Battery",
+        device_class=SensorDeviceClass.VOLTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT
+    )
+)
+
+SENSOR_TYPES_MAP = { description.key: description for description in SENSOR_TYPES }
+
+SENSOR_TYPES_KEYS = { description.key for description in SENSOR_TYPES }
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Configure a dispatcher connection based on a config entry."""
+
+    enabled_sensors = [s for s in entry.data.get(
+        CONF_SENSORS, []) if s in SENSOR_TYPES_KEYS]
+
+    @callback
+    def _receive_data(server, device, position):
+        for sensor_type in enabled_sensors:
+            sensor_id = f"{device.unique_id}-{sensor_type}"
+            if sensor_id in hass.data[DOMAIN][entry.entry_id][SENSORS]:
+                continue
+
+            hass.data[DOMAIN][entry.entry_id][SENSORS].add(sensor_id)
+
+            async_add_entities(
+                [TraccarSensorEntity(
+                    SENSOR_TYPES_MAP[sensor_type], server, device, position)]
+            )
+
+    async_dispatcher_connect(hass, TRACKER_UPDATE, _receive_data)
+
+
+class TraccarSensorEntity(SensorEntity, TraccarEntity):
+    """Represent a tracked device."""
+
+    def __init__(self, description, server, device, position):
+        """Set up Geofency entity."""
+        super().__init__(server, device)
+        self.entity_description = description
+        self._unique_id = f"{server}-{device.unique_id}-{description.key}"
+        self._update_traccar_info(device, position)
+
+    def _update_traccar_info(self, device, position):
+        self._attr_name = f"{device.name} {self.entity_description.name}"
+        if self.entity_description.key == KEY_BATTERY_LEVEL:
+            self._state = position.attributes.get(ATTR_BATTERY_LEVEL)
+        elif self.entity_description.key == KEY_BATTERY:
+            self._state = position.attributes.get(ATTR_BATTERY)
+
+    @property
+    def native_value(self):
+        """Return battery value of the device."""
+        return self._state
+
+    @property
+    def unique_id(self):
+        """Return the unique ID."""
+        return self._unique_id
+
+    async def async_added_to_hass(self) -> None:
+        await super().async_added_to_hass()
+
+        if state := await self.async_get_last_state():
+            self._state = state.state
